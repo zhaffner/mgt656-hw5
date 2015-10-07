f8b898c7d5eab05c16959eb71b11a0c1977a1358
zhaffner <zeev.haffner@yale.edu>
2015-10-07 20:18:29 +0000

    add nickname

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/nickname', function(request, response){
+    response.send('vast-pony');
+});
+
 app.listen(process.env.PORT || 4000);
